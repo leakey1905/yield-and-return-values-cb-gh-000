84e7046ac2949d9ec62cf928809becd8c13015ae
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 06:04:29 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    bingding.pry
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
-  array
+  collection
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
